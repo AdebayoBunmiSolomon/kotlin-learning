5d34104d2989e4757fcf99b79e7bcf57d7de3207
Adebayo Bunmi Solomon <61608290+AdebayoBunmiSolomon@users.noreply.github.com>
2026-02-09 10:01:19 +0100

    Learnt how to add icon and images to an android app

--- app/src/main/java/com/example/myfirstapp/MainActivity.kt
+++ app/src/main/java/com/example/myfirstapp/MainActivity.kt
@@ -1,12 +1,14 @@
 package com.example.myfirstapp
 
+import android.R
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -14,12 +16,16 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.systemBarsPadding
 import androidx.compose.foundation.layout.width
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 //import androidx.compose.ui.tooling.preview.Preview
@@ -46,25 +52,39 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String) {
-    Column(
-        modifier = Modifier
-            .fillMaxSize()
-            .background(Color.White)
-            .padding(vertical = 10.dp, horizontal = 8.dp),
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center
-    ) {
-        Text(
-            text = "Hello $name!",
-            color = Color.Blue,
-            fontSize = 20.sp,
-        )
-        Text(
-            text = "Hello some other text",
-            color = Color.Blue,
-            fontSize = 20.sp,
-        )
-    }
+    //to add icon to an android app
+    Icon(
+        imageVector = Icons.Default.Add,
+        contentDescription = null,
+        tint = Color.Blue
+    )
+
+    //to add image to an android app
+//    Image(
+//        painter = painterResource(id = R.drawable.ic_launcher_foreground),
+//        contentDescription = null,
+//        modifier = Modifier
+//            .background(color = Color.Red)
+//    )
+    //to understand ui
+//    Row(
+//        modifier = Modifier
+//            .fillMaxSize()
+//            .background(Color.White)
+//            .padding(vertical = 10.dp, horizontal = 8.dp),
+//        horizontalArrangement = Arrangement.Center,
+//    ) {
+//      Text(
+//            text = "Hello $name!",
+//            color = Color.Blue,
+//            fontSize = 20.sp,
+//        )
+//        Text(
+//            text = "Hello some other text",
+//            color = Color.Blue,
+//            fontSize = 20.sp,
+//        )
+//    }
 }
 
 //@Preview(showBackground = false)
