0de935abc6065cf81b4a7800445e7d24cf10ae04
Adebayo Bunmi Solomon <61608290+AdebayoBunmiSolomon@users.noreply.github.com>
2026-02-18 12:39:55 +0100

    learnt how to use state and clickable actions to update state

--- app/src/main/java/com/example/myfirstapp/MainActivity.kt
+++ app/src/main/java/com/example/myfirstapp/MainActivity.kt
@@ -5,22 +5,35 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import android.util.Log
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.systemBarsPadding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.myfirstapp.ui.theme.MyFirstAppTheme
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.text.style.TextAlign
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -31,10 +44,7 @@ class MainActivity : ComponentActivity() {
                 Scaffold(modifier = Modifier.fillMaxSize()
                     .padding(horizontal = 10.dp)
                     .systemBarsPadding()) { innerPadding ->
-                    Greeting(
-                        name = "Bunmi"
-                    )
-
+                    ActualApp()
                 }
             }
         }
@@ -42,7 +52,7 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
+fun Greeting() {
     LazyColumn(modifier = Modifier.fillMaxSize()) {
         items(count = 50) {
             Row(
@@ -54,7 +64,7 @@ fun Greeting(name: String) {
                         enabled = true,
                         onClickLabel = "Hello"
                     ) {
-                        val logMsg: String = "Hello"
+                        val logMsg = "Hello"
                         Log.d("Tag 1", logMsg)
                     }
             ) {
@@ -146,6 +156,37 @@ fun Greeting(name: String) {
 //            fontSize = 20.sp,
 //        )
 //    }
+}
+@Composable
+fun ActualApp() {
+    //learnt state and clickable actions for changing state
+    var count by remember { mutableStateOf(0) }
+    Column(
+        modifier = Modifier.background(color = Color.Transparent)
+            .fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = "You clicked the button $count times",
+            fontSize = 14.sp,
+            textAlign = TextAlign.Center
+        )
+        Button(onClick = {
+            if(count != 5){
+             count ++
+            }else{
+                count --
+            }
+        },
+            shape = RoundedCornerShape(6.dp),
+            colors = ButtonDefaults.buttonColors(
+                containerColor = Color.Magenta,
+                contentColor = Color.White
+            )) {
+            Text(text = "Click me!")
+        }
+    }
 }
 
 //@Preview(showBackground = false)
